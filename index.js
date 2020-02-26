736f84ee23339537d0e04928605902661c6859d0
Wei Sun <swyddz1989@gmail.com>
2020-02-25 16:40:45 -0800

    add links to zipcode

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ $(function() {
     pageLength: 50,
   });
   for (let z of zipInfo) {
+    z[0] = '<a href="https://google.com/maps?q=' + z[0] + '" target="_blank">' +
+        z[0] + '</a>';
     myTable.row.add(z);
   }
   myTable.draw();
