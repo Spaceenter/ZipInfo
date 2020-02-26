a6b1e1dc7c467a8cb67988b3ba308bf5b046236e
Wei Sun <swyddz1989@gmail.com>
2020-02-25 16:21:47 -0800

    default page length = 50

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $(function() {
-  let myTable = $('#my-table').DataTable({deferRender: true});
+  let myTable = $('#my-table').DataTable({
+    deferRender: true,
+    pageLength: 50,
+  });
   for (let z of zipInfo) {
     myTable.row.add(z);
   }
